f181d0f4fdf38d2f178af97380144132a03849f0
Bitcade1 <jack.england@bitcade.co.uk>
2026-01-11 12:10:04 +0000

    f

--- flask_app.py
+++ flask_app.py
@@ -4987,10 +4987,10 @@ def counting_3d_printing_parts():
         current_inventory = inventory_counts.get(part, 0)
         surplus = current_inventory - still_needed
 
-    if surplus < 0:
-        parts_status[part] = f"{-surplus} left to make"
-    else:
-        parts_status[part] = f"{surplus} extras"
+        if surplus < 0:
+            parts_status[part] = f"{-surplus} left to make"
+        else:
+            parts_status[part] = f"{surplus} extras"
 
     carpet_felt_parts = ["6ft Carpet", "7ft Carpet", "6ft Felt", "7ft Felt"]
     laminate_parts = list(LAMINATE_PART_NAMES)
